1d3345da83f0ed2a55f070a4bb8c6378bbee8151
My Name is Tito <github@mynameistito.com>
2026-08-01 17:45:50 +1200

    chore: ultracite init

---
+++ oxlint.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "oxlint";
+import core from "ultracite/oxlint/core";
+import react from "ultracite/oxlint/react";
+import vitest from "ultracite/oxlint/vitest";
+import jsPlugins from "ultracite/oxlint/js-plugins";
+const selectedJsPluginNames = new Set(["github","sonarjs","react-doctor"]);
+const selectedJsPluginRulePrefixes = new Set(["github","sonarjs","react-doctor"]);
+
+const selectedJsPlugins = {
+  ...jsPlugins,
+  jsPlugins: jsPlugins.jsPlugins?.filter((plugin) =>
+    selectedJsPluginNames.has(plugin.name)
+  ),
+  overrides: jsPlugins.overrides?.map((override) => ({
+    ...override,
+    rules: Object.fromEntries(
+      Object.entries(override.rules ?? {}).filter(([ruleName]) =>
+        selectedJsPluginRulePrefixes.has(ruleName.split("/")[0] ?? ruleName)
+      )
+    ),
+  })),
+  rules: Object.fromEntries(
+    Object.entries(jsPlugins.rules ?? {}).filter(([ruleName]) =>
+      selectedJsPluginRulePrefixes.has(ruleName.split("/")[0] ?? ruleName)
+    )
+  ),
+};
+
+export default defineConfig({
+  extends: [core, react, vitest, selectedJsPlugins],
+  ignorePatterns: core.ignorePatterns,
+});
